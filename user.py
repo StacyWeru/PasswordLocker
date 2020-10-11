b06f889a5f7b18ebcce9213ebb3a0a5df943c857
StacyWeru <stacyweru@gmail.com>
2020-10-12 01:56:32 +0300

    Modified class user

---
+++ user.py
@@ -0,0 +1,18 @@
+class User
+    '''
+    Create this class user to generate the user instance
+    '''
+
+user_list = []
+
+def __init__ (self,user_name,password)
+    self.user_name = user_name
+    self.password = password
+
+    def save_user(self)
+        '''
+        To create an instance of the user and save 
+        '''
+        User.user_list.append(self)
+
+
