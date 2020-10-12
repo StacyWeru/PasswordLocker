ebdfb1b8f6f6ded3e774a6faa5d60575d274f312
StacyWeru <stacyweru@gmail.com>
2020-10-12 03:42:35 +0300

    Created file and wrote some code' |

---
+++ generatePassword.py
@@ -0,0 +1,12 @@
+import string , random
+
+char = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"]
+
+def generatePassword(char):
+    generated = ""
+
+for i in range(char);
+    generated_char = random.choice(char)
+    gen_word = generated + generated_char
+
+    
