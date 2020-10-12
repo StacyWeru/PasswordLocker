d5c68c442485862bac1c3d1f7bd7304de1765b8d
StacyWeru <stacyweru@gmail.com>
2020-10-12 03:43:12 +0300

    Yet to identify what the problem is

--- user.py
+++ user.py
@@ -1,15 +1,16 @@
-class User
+class User:
+
     '''
     Create this class user to generate the user instance
     '''
 
-user_list = []
+    user_list = []
 
-def __init__ (self,user_name,password)
+def __init__ (self,user_name,password):
     self.user_name = user_name
     self.password = password
 
-    def save_user(self)
+    def save_user(self):
         '''
         To create an instance of the user and save 
         '''
