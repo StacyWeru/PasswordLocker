0aad0a67bca24b01b45f24c66f4f46ebbbf02e25
StacyWeru <stacyweru@gmail.com>
2020-10-12 08:32:26 +0300

    Tried to modify it appr.

--- user.py
+++ user.py
@@ -6,14 +6,49 @@ class User:
 
     user_list = []
 
-def __init__ (self,user_name,password):
+def __init__ (self,user_name,user_password):
     self.user_name = user_name
-    self.password = password
+    self.user_password = user_password
 
-    def save_user(self):
+def save_user(self):
         '''
         To create an instance of the user and save 
         '''
         User.user_list.append(self)
+
+def delete_user(self):
+
+    '''
+    deletes a saved ueser from user_lidt
+    '''
+
+    User.user_list.remove(self)
+
+@classmethod
+def user_exists (cls,password):
+
+    '''
+    Method that checks if user exists in user_list
+
+    Arg:
+        
+        password : Takes in password  to serach if it exists
+
+        returns: 
+        Boolean : True of flase depending if user exists
+
+    '''
+
+    for user in cls.user_list:
+        if user_password == password :
+            return True
+        else:
+            return False
+
+
+
+
+
+
 
 
